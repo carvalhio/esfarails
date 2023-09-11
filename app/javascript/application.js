634ec9a610cbecd4af22d645a7cac6f7918812ca
carvalhio <raffael.carvallho@gmail.com>
2023-09-11 12:10:13 -0300

    footer criado e personalizado

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// Adicione essas linhas no topo do seu arquivo application.js
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+// Importe o Slick Carousel após as linhas acima
+import 'slick-carousel';
--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,20 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+
+// Adicione essas linhas no topo do seu arquivo application.js
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+// Importe o Slick Carousel após as linhas acima
+import 'slick-carousel';
+
+// app/assets/javascripts/application.js
+$(document).ready(function () {
+    $("#scroll-to-top").click(function () {
+      $("html, body").animate({ scrollTop: 0 }, "slow");
+      return false;
+    });
+  });
+  
